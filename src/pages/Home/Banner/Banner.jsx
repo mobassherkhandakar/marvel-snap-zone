813dd8a70e24bd62854dfb2ce2913f635bebf8cc
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-21 20:59:52 +0600

    banner text change

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -7,14 +7,13 @@ const Banner = () => {
       <img src="https://wallpapers.com/images/hd/4k-marvel-digital-cartoon-t9z4pnuivn6488jq.jpg" className="w-full rounded-xl" />
       <div className="absolute flex items-center rounded-xl h-full left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
         <div className="text-white space-y-7 pl-12 w-1/2">
-          <h2 className="font-bold text-6xl">Affordable Price For Car Servicing</h2>
+          <h2 className="font-bold text-4xl">"Welcome to Snap-Zone: A World of Imagination and Play!"</h2>
           <p>
             There are many variations of passages of available, but the
             majority have suffered alteration in some form
           </p>
           <div>
           <button className="btn btn-active bg-[#FF3811] mr-5">Discover More</button>
-          <button className="btn btn-outline btn-secondary">Latest Project</button>
           </div>
         </div>
       </div>
@@ -31,14 +30,13 @@ const Banner = () => {
       <img src="https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700547904-optimized.jpg" className="w-full rounded-xl" />
       <div className="absolute flex items-center rounded-xl  h-full left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
         <div className="text-white space-y-7 pl-12 w-1/2">
-          <h2 className="font-bold text-6xl">Affordable Price For Car Servicing</h2>
+          <h2 className="font-bold text-4xl">"Welcome to Snap-Zone: A World of Imagination and Play!"</h2>
           <p>
             There are many variations of passages of available, but the
             majority have suffered alteration in some form
           </p>
           <div>
           <button className="btn btn-active bg-[#FF3811] mr-5">Discover More</button>
-          <button className="btn btn-outline btn-secondary">Latest Project</button>
           </div>
         </div>
       </div>
@@ -55,14 +53,13 @@ const Banner = () => {
       <img src="https://www.pixel4k.com/wp-content/uploads/2018/12/transformers-prime-4k_1546276362.jpg" className="w-full rounded-xl" />
       <div className="absolute flex items-center rounded-xl h-full left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
         <div className="text-white space-y-7 pl-12 w-1/2">
-          <h2 className="font-bold text-6xl">Affordable Price For Car Servicing</h2>
+          <h2 className="font-bold text-4xl">"Welcome to Snap-Zone: A World of Imagination and Play!"</h2>
           <p>
             There are many variations of passages of available, but the
             majority have suffered alteration in some form
           </p>
           <div>
           <button className="btn btn-active bg-[#FF3811] mr-5">Discover More</button>
-          <button className="btn btn-outline btn-secondary">Latest Project</button>
           </div>
         </div>
       </div>
